ae4335482c20f6efcbac491816c7375513843e16
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-08 01:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,9 @@
   cy.get('tr#loading', { timeout: 10000 }).should('be.visible').find('td').invoke('text').then(text => { 
     expect(text.trim()).equal("Loading..."); 
   }); 
+  cy.wait(4000); // Wait for promises to resolve and for loading to go away
+  cy.get("#output").find("tr", { timeout: 10000 }).then(rows => { // Increase timeout to 10 seconds
+    expect(rows.length).equal(4); 
+  }); 
+  // rest of your code...
 }
