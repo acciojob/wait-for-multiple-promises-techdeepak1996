9f66d902e785e777e0e9bf5e2523540f5c84cd11
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-14 01:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
   cy.visit(baseUrl + "/main.html"); 
   cy.get('#output').find('td', { timeout: 10000 }).should('contain.text', 'Loading...'); 
   cy.get('#output').find('tr', { timeout: 20000 }).should('have.length', 4); 
+  cy.get('#output > tr > td:nth-child(1)').each(($elm, index) => {
+    const text = $elm.text();
+    if (text.includes('Promise')) {
+      cy.get('#output > tr > td:nth-child(1)').eq(index).next().then(($nextElm) => {
+        const nextText = parseInt($nextElm.text());
+        expect(nextText).to.be.at.least(1);
+        expect(nextText).to.be.at.most(3);
+      });
+    } else if (text.includes('Total')) {
+      cy.get('#output > tr > td:nth-child(1)').eq(index).next().then(($nextElm) => {
+        const nextText = parseInt($nextElm.text());
+        expect(nextText).to.be.at.least(2);
+        expect(nextText).to.be.at.most(4);
+      });
+    }
+  });
 }
